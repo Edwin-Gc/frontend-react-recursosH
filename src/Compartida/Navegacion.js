06b2558ddfffadab7fd098076b033bc5a019409e
Edwin-Gc <bytevisiondesign@gmail.com>
2025-05-19 21:18:45 -0500

    Build y código fuente listos para producción

--- src/Compartida/Navegacion.js
+++ src/Compartida/Navegacion.js
@@ -1,36 +1,48 @@
-
 import React from "react";
-import { Link } from "react-router-dom";
-import "./Navegacion.css"; // Importa los estilos
+import { Link, useLocation } from "react-router-dom";
+import "./Navegacion.css";
 
 export default function Navegacion() {
+  const location = useLocation();
+
   return (
     <div className="container my-3">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-secondary nav-custom">
+      <nav className="navbar navbar-expand-lg bg-primary nav-custom">
         <div className="container-fluid">
           <button
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a className="navbar-brand" href="/">
+
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <a className="navbar-brand text-white" href="/">
               Recursos Humanos
             </a>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === "/" ? "active" : ""
+                  }`}
+                  to="/"
+                >
                   Inicio
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/agregar">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === "/agregar" ? "active" : ""
+                  }`}
+                  to="/agregar"
+                >
                   Agregar Empleado
                 </Link>
               </li>
